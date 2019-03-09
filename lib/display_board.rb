2cc44b2ad2e1e2271be0c95dc19d50759e2b4105
BEAU WITKA <bwitka@gmail.com>
2019-03-09 03:01:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 
 def display_board(board)
-  puts " #{b[0]} | #{b[1]} | #{b[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{b[3]} | #{b[4]} | #{b[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{b[6]} | #{b[7]} | #{b[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
